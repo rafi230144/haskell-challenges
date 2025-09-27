8a4984e704c653a34a428b395b7781253d4dfc61
rafi230144 <raa2200@columbia.edu>
2025-09-27 06:00:15 -0700

    Implemented solution; all tests run in ~0.33s on my machine

--- j1-lazy-foldrM/src/Lib.hs
+++ j1-lazy-foldrM/src/Lib.hs
@@ -1,6 +1,39 @@
-module Lib
-    ( foldrM
-    ) where
+{-# LANGUAGE Haskell2010
+  , GADTSyntax
+  , LambdaCase
+  , ScopedTypeVariables
+#-}
 
-foldrM :: (Foldable t, Monad m) => (a -> b -> m b) -> b -> t a -> m b
-foldrM = undefined
+module Lib
+  ( foldrM
+  ) where
+
+import Data.Foldable
+  ( toList
+  , foldlM
+  )
+
+data Peano where
+    Zero :: Peano
+    Succ :: Peano -> Peano
+
+take' :: forall a. Peano -> [a] -> [a]
+take' = \cases
+    Zero _               -> []
+    (Succ n') ~(a : sa') -> a : take' n' sa'
+
+reverse' :: forall a. [a] -> [a]
+reverse' =
+    let reverse'A = \ n ra -> \case
+            []      -> (n, ra)
+            a : sa' -> case reverse'A n (a : ra) sa' of
+                ~(n', ra') -> (Succ n', ra')
+    in  \sa ->
+            let (n, ra) = reverse'A Zero [] sa
+            in  take' n ra
+
+foldrM :: forall m t a b.
+    (Foldable t, Monad m) =>
+    (a -> b -> m b) -> b -> t a -> m b
+foldrM = \ g b ->
+    foldlM (flip g) b . reverse' . toList
